276fdd17221d4d8bb74cf0e56323f8c65b7219a7
kiryl-z <rompostbox@gmail.com>
2021-05-19 15:28:30 +0300

    get json return ok

--- src/main/java/com/kafka/demo/controllers/MessageController.java
+++ src/main/java/com/kafka/demo/controllers/MessageController.java
@@ -9,7 +9,7 @@ public class MessageController {
 
 //annotation ensures that HTTP GET requests to /greeting are mapped to the greeting() method.
     @PostMapping("/kafka")
-    public String addNewMessage(@RequestBody MessageBody messageBody) {
+    public String postControler(@RequestBody MessageBody messageBody) {
         return "ok";
     }
 
