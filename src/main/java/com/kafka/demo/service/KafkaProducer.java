8db17d7417ed070569b93dd5ceeca22ebee73ed1
Kiryl Zeliankou <kiryl.zeliankou@bglgroup.co.uk>
2021-05-25 19:54:06 +0300

    Changed topic from hardcode to configuration

--- src/main/java/com/kafka/demo/service/KafkaProducer.java
+++ src/main/java/com/kafka/demo/service/KafkaProducer.java
@@ -3,6 +3,7 @@ package com.kafka.demo.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +11,12 @@ import org.springframework.stereotype.Service;
 public class KafkaProducer {
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
-    private static final String topic = "restcontrollertest";
+
+    @Value("${spring.kafka.topic.name}")
+    private String topic;
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
-
 
     public void SendMessage(String message) {
         logger.info(String.format("#### -> Producing message -> %s", message));
