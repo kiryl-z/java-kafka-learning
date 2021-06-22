be20a8f131a464bc961d42ed5add1ebe6681b028
Kiryl Zeliankou <kiryl.zeliankou@bglgroup.co.uk>
2021-06-22 10:18:23 +0300

    Removed consumer and configured docker compose

--- src/main/java/com/kafka/demo/ProducerApp.java
+++ src/main/java/com/kafka/demo/ProducerApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.kafka.demo.controllers")
 public class ProducerApp {
     public static void main(String[] args) {
         SpringApplication.run(ProducerApp.class, args);
